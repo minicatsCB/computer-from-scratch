c9b18db3423cab50ace6b372bc53e1391ce38320
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-06-20 10:21:00 +0200

    Add new units

--- chips/compile.sh
+++ chips/compile.sh
@@ -6,7 +6,7 @@ FILES=("not1w1b_chip" "and2w1b_chip" "or2w1b_chip" "xor2w1b_chip"
        "or16w1b_chip"
        "mux2w1b_chip" "mux2w16b_chip" "mux4w16b_chip" "mux8w16b_chip"
        "demux2w1b_chip" "demux4w1b_chip" "demux8w1b_chip"
-       "halfadder2w1b_chip" "fulladder2w1b_chip")
+       "halfadder2w1b_chip" "fulladder2w1b_chip" "adder2w16b_chip" "inc1w16b_chip")
 
 for t in ${FILES[@]}; do
     bash ./start.sh -n $t -sae
