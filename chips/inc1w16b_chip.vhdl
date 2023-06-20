3fbc92719b967f3a3bfdb3759534a05f0f8b002e
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-06-20 10:23:59 +0200

    Move magic number to a variable

--- chips/inc1w16b_chip.vhdl
+++ chips/inc1w16b_chip.vhdl
@@ -13,14 +13,14 @@ end entity INC1W16B_CHIP;
 
 architecture RTL of INC1W16B_CHIP is
 
-  signal carry : std_logic_vector(15 downto 0);
+  constant step : std_logic_vector := "0000000000000001";
 
 begin
 
   U0 : entity work.adder2w16b_chip(rtl)
     port map (
       A => A,
-      B => "0000000000000001",
+      B => step,
       O => O
     );
 
