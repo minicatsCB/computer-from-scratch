a570e4da18b769d3b01f509c60b594f0718917b2
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-09-07 18:17:03 +0200

    refs #9: improve test logs

--- chips/and2w16b_chip_tb.vhdl
+++ chips/and2w16b_chip_tb.vhdl
@@ -1,6 +1,9 @@
 library IEEE;
   use ieee.std_logic_1164.all;
 
+library work;
+  use work.utils_package.to_string;
+
 entity AND2W16B_CHIP_TB is
 end entity AND2W16B_CHIP_TB;
 
@@ -28,24 +31,35 @@ begin
     );
 
   STIMULUS : process is
+    type pattern_type is record
+      a : std_logic_vector(15 downto 0);
+      b : std_logic_vector(15 downto 0);
+      o : std_logic_vector(15 downto 0);
+    end record pattern_type;
+
+    type pattern_array is array (natural range <>) of pattern_type;
+
+    constant patterns : pattern_array :=
+    (
+      1 => ("0000000000000000", "1111111111111111", "0000000000000000"),
+      2 => ("0001110001010100", "0001110101011001", "0001110001010000")
+    );
+
   begin
 
-    a <= "0000000000000000";
-    b <= "1111111111111111";
-    wait for 50 ns;
-    assert o = "0000000000000000"
-      report "Expected: o = (a nand b) | Received: o = other result"
-      severity failure;
+    for i in patterns'range loop
+      a  <= patterns(i).a;
+      b  <= patterns(i).b;
+      wait for 50 ns;
+      assert o = patterns(i).o
+        report "[Error] o[" & integer'image(i) & "] >>> Expected: " & to_string(patterns(i).o) & " / Received: " & to_string(o)
+        severity failure;
 
-    a <= "0001110001010100";
-    b <= "0001110101011001";
-    wait for 50 ns;
-    assert o = "0001110001010000"
-      report "Expected: o = (a nand b) | Received: o = other result"
-      severity failure;
+    end loop;
 
-    assert true
-      report "Tests finished";
+    assert false
+      report "Tests finished"
+      severity note;
     wait;
 
   end process STIMULUS;
