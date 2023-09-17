b92ce7e92302b7595d39e2d07c704b5ee8afb889
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-09-17 16:52:58 +0200

    refs #9: improve test logs

--- chips/mux2w16b_chip_tb.vhdl
+++ chips/mux2w16b_chip_tb.vhdl
@@ -1,5 +1,8 @@
 library IEEE;
   use ieee.std_logic_1164.all;
+  
+library work;
+  use work.utils_package.to_string;
 
 entity MUX2W16B_CHIP_TB is
 end entity MUX2W16B_CHIP_TB;
@@ -17,8 +20,8 @@ architecture BEHAVIOR of MUX2W16B_CHIP_TB is
 
   signal a   : std_logic_vector(15 downto 0);
   signal b   : std_logic_vector(15 downto 0);
-  signal o   : std_logic_vector(15 downto 0);
   signal sel : std_logic;
+  signal o   : std_logic_vector(15 downto 0);
 
 begin
 
@@ -31,42 +34,40 @@ begin
     );
 
   STIMULUS : process is
+
+    type pattern_type is record
+      a   : std_logic_vector(15 downto 0);
+      b   : std_logic_vector(15 downto 0);
+      sel : std_logic;
+      o   : std_logic_vector(15 downto 0);
+    end record pattern_type;
+
+    type pattern_array is array (natural range <>) of pattern_type;
+
+    constant patterns : pattern_array :=
+    (
+      1 => ("0000000000000000", "1111111111111111", '0', "0000000000000000"),
+      2 => ("0000000000000000", "1111111111111111", '1', "1111111111111111"),
+      3 => ("0001110001010100", "0001110101011001", '0', "0001110001010100"),
+      4 => ("0001110001010100", "0001110101011001", '1', "0001110101011001")
+    );
+
   begin
 
-    a   <= "0000000000000000";
-    b   <= "1111111111111111";
-    sel <= '0';
-    wait for 50 ns;
-    assert o = "0000000000000000"
-      report "Expected: o = 0000000000000000 | Received: o = other result"
-      severity failure;
+    for i in patterns'range loop
+      a  <= patterns(i).a;
+      b  <= patterns(i).b;
+      sel  <= patterns(i).sel;
+      wait for 50 ns;
+      assert o = patterns(i).o
+        report "[Error] o[" & integer'image(i) & "] >>> Expected: " & to_string(patterns(i).o) & " / Received: " & to_string(o)
+        severity failure;
 
-    a   <= "0000000000000000";
-    b   <= "1111111111111111";
-    sel <= '1';
-    wait for 50 ns;
-    assert o = "1111111111111111"
-      report "Expected: o = 0000000000000000 | Received: o = other result"
-      severity failure;
+    end loop;
 
-    a   <= "0001110001010100";
-    b   <= "0001110101011001";
-    sel <= '0';
-    wait for 50 ns;
-    assert o = "0001110001010100"
-      report "Expected: o = 0000000000000000 | Received: o = other result"
-      severity failure;
-
-    a   <= "0001110001010100";
-    b   <= "0001110101011001";
-    sel <= '1';
-    wait for 50 ns;
-    assert o = "0001110101011001"
-      report "Expected: o = 0000000000000000 | Received: o = other result"
-      severity failure;
-
-    assert true
-      report "Tests finished";
+    assert false
+      report "Tests finished"
+      severity note;
     wait;
 
   end process STIMULUS;
