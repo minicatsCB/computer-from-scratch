4229d4ee0f06ea49bd4155e19aca25718677f52a
minicatsCB <19836547+minicatsCB@users.noreply.github.com>
2023-09-17 16:44:39 +0200

    refs #9: improve test logs

--- chips/inc1w16b_chip_tb.vhdl
+++ chips/inc1w16b_chip_tb.vhdl
@@ -1,6 +1,9 @@
 library IEEE;
   use ieee.std_logic_1164.all;
 
+library work;
+  use work.utils_package.to_string;
+
 entity INC1W16B_CHIP_TB is
 end entity INC1W16B_CHIP_TB;
 
@@ -25,34 +28,36 @@ begin
     );
 
   STIMULUS : process is
+
+    type pattern_type is record
+      a       : std_logic_vector(15 downto 0);
+      o       : std_logic_vector(15 downto 0);
+    end record pattern_type;
+
+    type pattern_array is array (natural range <>) of pattern_type;
+
+    constant patterns : pattern_array :=
+    (
+      1 => ("0000000000000000", "0000000000000001"),
+      2 => ("1111111111111111", "0000000000000000"),
+      3 => ("0000000000000101", "0000000000000110"),
+      4 => ("1111111111111011", "1111111111111100")
+    );
+
   begin
 
-    a <= "0000000000000000";
-    wait for 50 ns;
-    assert o = "0000000000000001"
-      report "Expected: o = 0000000000000001 | Received: o = other result"
-      severity failure;
+    for i in patterns'range loop
+      a  <= patterns(i).a;
+      wait for 50 ns;
+      assert o = patterns(i).o
+        report "[Error] o[" & integer'image(i) & "] >>> Expected: " & to_string(patterns(i).o) & " / Received: " & to_string(o)
+        severity failure;
 
-    a <= "1111111111111111";
-    wait for 50 ns;
-    assert o = "0000000000000000"
-      report "Expected: o = 0000000000000000 | Received: o = other result"
-      severity failure;
+    end loop;
 
-    a <= "0000000000000101";
-    wait for 50 ns;
-    assert o = "0000000000000110"
-      report "Expected: o = 0000000000000110 | Received: o = other result"
-      severity failure;
-
-    a <= "1111111111111011";
-    wait for 50 ns;
-    assert o = "1111111111111100"
-      report "Expected: o = 1111111111111100 | Received: o = other result"
-      severity failure;
-
-    assert true
-      report "Tests finished";
+    assert false
+      report "Tests finished"
+      severity note;
     wait;
 
   end process STIMULUS;
